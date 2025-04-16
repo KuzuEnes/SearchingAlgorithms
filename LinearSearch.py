11c78e0b2b4a61313f6653eb02499563f3f82ad2
Enes K. <eneskuzu2003@gmail.com>
2025-04-16 18:44:16 +0300

    LinearSearch eklendi.

---
+++ LinearSearch.py
@@ -0,0 +1,21 @@
+def linear_search(arr, hedef):
+    for i in range(len(arr)):
+        if arr[i] == hedef:
+            return i  # hedef bulunduğunda indeks döndürülür
+    return -1  # hedef bulunamazsa -1 döndürülür
+
+# Örnek kullanım
+sayilar = [5, 3, 7, 1, 9, 4]
+hedef = 9
+sonuc = linear_search(sayilar, hedef)
+
+if sonuc != -1:
+    print(f"{hedef} dizide {sonuc}. indexte bulundu.")
+else:
+    print(f"{hedef} dizide bulunamadı.")
+
+#Time Complexity: O(n)
+#Space Complexity : O(1)
+
+#Lineer arama, en temel arama algoritmasıdır. Bu algoritma, bir diziyi baştan sona tarar ve hedef öğeyi bulana kadar her öğeyi karşılaştırır. Eğer hedef öğe bulunursa, öğenin indisini döndürür.
+
