ac60825b9543e01d97e8a8b06e5e8c39c88fcc1b
Enes K. <eneskuzu2003@gmail.com>
2025-04-23 22:31:22 +0300

    LinearSearch.py güncellendi.

--- LinearSearch.py
+++ LinearSearch.py
@@ -18,4 +18,4 @@ else:
 #Space Complexity : O(1)
 
 #Lineer arama, en temel arama algoritmasıdır. Bu algoritma, bir diziyi baştan sona tarar ve hedef öğeyi bulana kadar her öğeyi karşılaştırır. Eğer hedef öğe bulunursa, öğenin indisini döndürür.
-
+#Lineer arama, küçük ve sıralı olmayan veri listelerinde eleman aramak için kullanılır.
